5985864a9350ae842dfa8bca9484e9e63cb67587
Rahmat <thisisrahmat@gmail.com>
2024-02-06 16:13:32 +0000

    rebuilt to a good standard

---
+++ RahmatOS/src/config.ts
@@ -0,0 +1,50 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://thisisrahmat.com/", // replace this with your deployed domain
+  author: "Sat Naing",
+  desc: "The personal site of Rahmat Junaid",
+  title: "Rahmat",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
+
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: true,
+  svg: false,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/thisisrahmat",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/rahmat-junaid/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:thisisrahmat@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: false,
+  },
+
+
+
+
+];
