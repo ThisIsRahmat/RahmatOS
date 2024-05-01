55c90c2d4188810a4228af77b9cdacad7151749b
thisisrahmat <thisisrahmat@gmail.com>
2024-05-01 13:11:01 +0100

    added blog post

---
+++ RahmatOS/src/components/PostContent.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+const PostContent = ({ post }) => {
+  const { content, slug, publishedAt } = post;
+
+  const dateString = new Date(publishedAt).toLocaleDateString("en-GB", {
+    dateStyle: "full",
+  });
+  const timeString = new Date(publishedAt).toLocaleTimeString("en-GB", {
+    timeStyle: "short",
+  });
+
+  return (
+    <div >
+        <div className="pb-8">
+      <time dateTime={publishedAt}>
+        {dateString} {timeString}
+      </time>
+      </div>
+      {/* <ReactMarkdown>{content.markdown}</ReactMarkdown> */}
+      <ReactMarkdown className="gap-x-2" children={content.markdown} />
+
+ 
+    </div>
+  );
+};
+
+export default PostContent;
