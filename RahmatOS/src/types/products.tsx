87931c992e180dc02a58253f503f3ed7e79f9e3d
thisisrahmat <thisisrahmat@gmail.com>
2024-12-06 20:26:53 +0000

    fixing project type error

--- RahmatOS/src/types/products.tsx
+++ RahmatOS/src/types/products.tsx
@@ -3,10 +3,10 @@ import { StaticImageData } from "next/image";
 export type Product = {
   title: string;
   description: string;
-  thumbnail: StaticImageData;
-  images: StaticImageData[] | string[];
+  thumbnail?: StaticImageData;
+  images?: StaticImageData[] | string[];
   href: string;
   slug?: string;
   stack?: string[];
-  content?: React.ReactNode | string;
+  content: React.ReactNode | string;
 };
