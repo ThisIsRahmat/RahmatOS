65a668209ed53bbe08d0b3ed09076f904e7e7963
thisisrahmat <thisisrahmat@gmail.com>
2025-07-16 00:23:53 +0100

    day 2 of 100 days of code

--- RahmatOS/src/constants/til.tsx
+++ RahmatOS/src/constants/til.tsx
@@ -12,6 +12,15 @@ export const habits: HabitEntry[] = [
     time: 40,
     task: " Worked on the execute program Python beginners course as a refresher for around 20 minutes, then spent the remainder of the time working on InterviewGenie - I am migrating the auth from Stytch to Clerk so I am currently in the trenches of refining my code. Today I focused on the frotend and deleting the Stytch auth code. ",
     
+  },
+
+  {
+    dayNumber: 2,
+    date: "2025-07-15",
+    time: 20,
+    task:"Today was a bit of a lazy day - I just spent around 20 mins going through ExecuteProgram python course - today was about Tuples and List methods"
+    
+    
   }
 
 ];
