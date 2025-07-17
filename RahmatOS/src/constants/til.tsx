7632208e77e72c7c02b1aac31a400b10d4d13e2c
thisisrahmat <thisisrahmat@gmail.com>
2025-07-18 00:59:04 +0100

    til updated

--- RahmatOS/src/constants/til.tsx
+++ RahmatOS/src/constants/til.tsx
@@ -21,6 +21,27 @@ export const habits: HabitEntry[] = [
     task:"Today was a bit of a lazy day - I just spent around 20 mins going through ExecuteProgram python course - today was about Tuples and List methods"
     
     
-  }
+  },
+
+  {
+  dayNumber: 3,
+  date: "2025-07-16",
+  time: 5,
+  task:"I spent around 5 minutes going through the ExecuteProgram python course - today was about Dictionaries"
+  
+  
+  
+  
+},
+
+{
+dayNumber: 4,
+date: "2025-07-17",
+time: 120,
+task:" I started the cs50 x AI course but then I just wasn't into it, too much dialogue and not engouh hands-on work so I started the Fast.ais DeepLearnign for coders course and completed the first section. It was great, I learned about the basics fo creating a model and I'm super excited to dive in more. I have a bunch of ides of what to build."
+
+
+
+}
 
 ];
